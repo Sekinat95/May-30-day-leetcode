bebaf03dcd5de877980eef73eb73eb6a028b9a69
sekinat95 <sekinat95@gmail.com>
2020-05-14 20:56:19 +0200

    remove k digits to leave the smallest num possible

---
+++ day13.py
@@ -0,0 +1,16 @@
+#remove k digits such that the remaining digits makes the smallest num
+def removeKdigits(self, num: str, k: int):
+  stack = []
+  number_to_remove = k
+  for current_element in num:
+    while number_to_remove and stack and stack[-1] > current_element:
+      stack.pop()
+      number_to_remove = number_to_remove-1
+    stack.append(current_element)
+  answer = "".join(stack[0:len(num)-k]).lstrip("0") #lstrip removes leading zeros
+  if len(answer):
+    return answer
+  else:
+    return "0"
+
+    
